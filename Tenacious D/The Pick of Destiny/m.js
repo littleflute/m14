33b1cebe8e3364ae9d0165ff6d88a0c44c86f213
littleflute <13699175164@sina.cn>
2017-06-24 17:40:32 -0500

    Add files via upload

---
+++ Tenacious D/The Pick of Destiny/m.js
@@ -0,0 +1,17 @@
+var f = [];				
+f[0]	= "v0.0.9";			
+f[1]	= "01 Kickapoo.mp3";		
+f[2]	= "02 Classico.mp3";		
+f[3]	= "03 Baby.mp3";		
+f[4]	= "04 Destiny.mp3";		
+f[5]	= "05 History.mp3";		
+f[6]	= "06 The Government Totally Sucks.mp3";		
+f[7]	= "07 Master Exploder.mp3";		
+f[8]	= "08 The Divide.mp3";		
+f[9]	= "09 Papagenu (He's My Sassafrass).mp3";		
+f[10]	= "10 Dude (I Totally Miss You).mp3";		
+f[11]	= "11 Break In-City (Storm the Gate).mp3";		
+f[12]	= "12 Car Chase City.mp3";		
+f[13]	= "13 Beelzeboss (The Final Showdown).mp3";		
+f[14]	= "14 Pod.mp3";		
+f[15]	= "15 The Metal.mp3";		
